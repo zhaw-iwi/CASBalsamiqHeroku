2a743f2714b6861dadbd03a7372121789ec71c2d
Adrian Moser <mosa@zhaw.ch>
2021-02-12 11:22:24 +0100

    php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
- <?php echo '<p>Hallo Welt</p>'; ?>
+<?
+
+$files = glob(dirname(__FILE__) . "/*.pdf");
+
+if (count($files) == 1) {
+	header($files[0]);
+} else {
+	println("Achtung, keine oder mehr als eine PDF Datei :-(");
+	println $files;
+}
+
+?>
