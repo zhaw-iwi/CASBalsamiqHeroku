4d2b3ea977b59326203ce42dc196f48727567aca
Adrian Moser <mosa@zhaw.ch>
2021-02-12 11:40:25 +0100

    php

--- index.php
+++ index.php
@@ -5,11 +5,13 @@ $files = glob(dirname(__FILE__) . "/*.pdf");
 print_r($files);
 print(count($files));
 
-/** if (count($files) == 1) {
-	header($files[0]);
+if (count($files) == 1) {
+	$url = 'http://'.$_SERVER['HTTP_HOST'];
+	$filename = basename[$files[0]);
+	header("Location: " . $url . "/" . $filename);
 } else {
 	println("Achtung, keine oder mehr als eine PDF Datei :-(");
 	println($files);
-}*/
+}
 
 ?>
