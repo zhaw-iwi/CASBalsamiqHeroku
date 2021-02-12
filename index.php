d4529be705c72ad4f059dadd81882189a1f52e8b
Adrian Moser <mosa@zhaw.ch>
2021-02-12 11:31:12 +0100

    php

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 
 $files = glob(dirname(__FILE__) . "/*.pdf");
 
-if (count($files) == 1) {
+print $files;
+
+/** if (count($files) == 1) {
 	header($files[0]);
 } else {
 	println("Achtung, keine oder mehr als eine PDF Datei :-(");
 	println($files);
-}
+}*/
 
 ?>
